976daaae7ddde6990c710c33b20a414b546ad762
Miguel Lammens <justin.bugtong@gmail.com>
2020-05-26 20:09:19 +0200

    catering vormgeving

--- catering.php
+++ catering.php
@@ -12,7 +12,7 @@
     $BD = date('Y-m-d', strtotime("now"));
     $ED = date('Y-m-d', strtotime("+1 week"));
 
-    $order = new Orders();
+    $order = new Order();
     $order->setBD($BD);
     $order->setED($ED);
     $result = $order->getOrders();
@@ -34,27 +34,60 @@
 <head>
     <title>Lab 2</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="style/reset.css">
-    <link rel="stylesheet" type="text/css" href="style/screen.css">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/screen.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta name="theme-color" content="#272727">
 </head>
 
 <body>
+<div class="topbar">
+    <a href="index.php"><div class="logo"> </div></a>
+    <p class="title">Foodcart - Bestellen</p>
+    <a href="profile.php"><div class="account"><img src="images/user.svg" /> </div> </a>
+  </div>
 
-<?php foreach($result as $r): ?>
+  <div class="sidebar">
+      
+      <div class="nav">
+        <a href="index.php"> <p class="selected"> <img src="images/dashboard2.svg" />  Overview</p> </a>
+        <a href="orders.php"> <p> <img src="images/order.svg"/>  Orders</p> </a>
+        <a href="clients.php"> <p> <img src="images/support.svg"/>  Clients</p> </a>
+        <a href="menus.php"> <p> <img src="images/food.svg"/>  Menus</p> </a>
+      </div>
+
+      <div class="logout">
+      <a href="logout.php"> <p> <img src="images/logout.svg"/>  Logout</p> </a>
+      </div>
+  </div>
+
+  <div class="container">
+  
+  <?php foreach($result as $r): ?>
     <?php 
-echo "<p>".$r["COUNT(id)"]."</p><br>";
-echo "<p>".$r['order']."</p><br>";
-echo "<p>".$r['school']."</p><br>";
-echo "<p>".$r['deliverydate']."</p><br>";
-
-
+    echo "<ul class='cateringul'>";
+    echo "<il>";
+    echo "<h1>".$r['deliverydate']."</h1>";
+    echo "</li>";
+    echo "<il>";
+    echo "<h2>".$r['school']."</h2>";
+    echo "</li>";
+    echo "<il class ='lititle'>";
+    echo $r['order'];
+    echo "</li>";
+    echo "<il class='count'>";
+    echo $r["COUNT(id)"];
+    echo "</li>";
+    echo "</ul>";
 
  ?>
 
 
     <?php endforeach;?>
+  
+  
+  </div>
+
 </body>
 
 
--- classes/user.class.php
+++ classes/user.class.php
@@ -120,7 +120,7 @@
 
             try {
                 //$conn = Db::getInstance();
-                $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","root", null);
+                $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","", null);
                 $statement = $conn->prepare("INSERT into user (email,pass, naam, voornaam,school) VALUES(:email, :password, :naam, :voornaam, :school)");
                 $statement->bindParam(":email",$this->email);
                 $statement->bindParam(":naam",$this->naam);
@@ -140,7 +140,7 @@
         
         try{
             //$conn = Db::getInstance();
-            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","root", null);
+            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","", null);
             $statement = $conn->prepare("select * from user where email = :email");
             
             //parameter binden
@@ -174,7 +174,7 @@
 
     public function getCrdt(){
         try{
-            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","root", null);
+            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","", null);
             $statement = $conn->prepare("select credit from user where id = :userid");
             
             //parameter binden
@@ -189,7 +189,7 @@
     }
     public function payment(){
         try{
-            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","root", null);
+            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","", null);
             $statement = $conn->prepare("update user set credit = :cr where id = :userid");
             
             //parameter binden
@@ -213,7 +213,7 @@
 
         try {
             //$conn = Db::getInstance();
-            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","root", null);
+            $conn= new PDO("mysql:host=localhost;dbname=lab2;","root","", null);
             $statement = $conn->prepare("update user set email = :email, naam = :naam, voornaam = :voornaam, pass = :password, school = :school where id = :userid");
             $statement->bindParam(":email",$this->email);
             $statement->bindParam(":naam",$this->naam);
